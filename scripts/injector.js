13be3cec4b412e61373c68c672389a33bb563655
Your Name <you@example.com>
2020-12-30 13:40:05 +0800

    .

--- scripts/injector.js
+++ scripts/injector.js
@@ -4,3 +4,7 @@ hexo.extend.injector.register('body_end', `
   <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
   <script defer src="/js/aplayer.js"></script>
 `);
+
+hexo.extend.injector.register('body_end', `
+  <script src="https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/qipao.js"></script>
+`);
